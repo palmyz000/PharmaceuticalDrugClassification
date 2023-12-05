8fe2fa3910a247872ee12549ce052ae2036082eb
Suphawit meesak <meepalm32@gmail.com>
2023-12-05 19:53:59 +0700

    new update

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import torch
+import torchvision.transforms as T
+from PIL import Image
+import numpy as np
+
+# Load the model
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+model = torch.load('mobilenetv3_large_100_checkpoint_fold2.pt', map_location=device)
+model.eval()
+
+# Set the image transformation
+image_size = (224, 224)  # You can adjust the size as needed
+image_transform = T.Compose([
+    T.Resize(image_size),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Set title and header
+st.title('Pharmaceutical Drugs Classification')
+st.header('Please upload an image')
+
+# Image upload and prediction
+uploaded_image = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image).convert('RGB')
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    
+    if st.button('Predict'):
+        with torch.no_grad():
+            # Preprocess the image
+            img_tensor = image_transform(image).unsqueeze(0)
+            img_tensor = img_tensor.to(device)
+
+            # Get prediction
+            output = model(img_tensor)
+            probabilities = torch.nn.functional.softmax(output[0], dim=0)
+            predicted_class = torch.argmax(probabilities).item()
+            class_names = ['Alaxan', 'Bactidol', 'Decolgen', 'Fish_Oil', 'Kremil_S']  # Update with your class names
+
+            # Display prediction
+            st.write(f"Predicted Class: {class_names[predicted_class]}")
+            st.write(f"Probability: {probabilities[predicted_class].item()*100:.2f}%")
--- prediction.py
+++ prediction.py
@@ -1,44 +1,51 @@
+## Making Pridcition return class & prob
 from typing import List, Tuple
 import torch
 import torchvision.transforms as T
+
 from PIL import Image
-
 def pred_class(model: torch.nn.Module,
-               image,
-               class_names: List[str],
-               image_size: Tuple[int, int] = (224, 224),
-               ):
-    # Check device availability
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-    # Move model and input image to the selected device
-    model.to(device)
-
-    # Create image transformation
-    image_transform = T.Compose([
-        T.Resize(image_size),
-        T.ToTensor(),
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-
-    # Open image
+                        image,
+                        class_names: List[str],
+                        image_size: Tuple[int, int] = (224, 224),
+                        ):
+    
+    
+    # 2. Open image
     img = image
 
-    # Transform image and send it to the target device
-    transformed_image = image_transform(img).unsqueeze(dim=0).float()
-    transformed_image = transformed_image.to(device)
+    # 3. Create transformation for image (if one doesn't exist)
+    image_transform = T.Compose([
+            T.Resize(image_size),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+    
 
-    # Set the model to evaluation mode and make predictions
+    ### Predict on image ### 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # 4. Make sure the model is on the target device
+    model.to(device)
+    
+
+    # 5. Turn on model evaluation mode and inference mode
     model.eval()
     with torch.inference_mode():
-        target_image_pred = model(transformed_image)
+      # 6. Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+      transformed_image = image_transform(img).unsqueeze(dim=0).float()
 
-    # Convert logits to prediction probabilities
+      # 7. Make a prediction on image with an extra dimension and send it to the target device
+      target_image_pred = model.forward(transformed_image.to(device))
+
+    # 8. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
     target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
 
-    # Convert prediction probabilities to labels
+    # 9. Convert prediction probabilities -> prediction labels
     target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
-    classname = class_names[target_image_pred_label]
+
+    classname =  class_names[target_image_pred_label]
     prob = target_image_pred_probs.cpu().numpy()
 
     return prob
