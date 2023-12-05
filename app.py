2d24cd43f495d4c61338b1df08c510076cbb0988
Suphawit meesak <meepalm32@gmail.com>
2023-12-05 17:48:22 +0700

    new update

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import streamlit as st
-import tensorflow as tf
-from PIL import Image, ImageOps
+import torch
+from torchvision import transforms
+from PIL import Image
 import numpy as np
 
 # โหลดโมเดล
-model = tf.keras.models.load_model('mobilenetv3_large_100_checkpoint_fold2.pt')  # แทนที่ด้วยตำแหน่งที่เก็บโมเดลของคุณ
+model = torch.load('mobilenetv3_large_100_checkpoint_fold2.pt', map_location=torch.device('cpu'))  # แทนที่ด้วยตำแหน่งที่เก็บโมเดลของคุณ
 
 st.title('CNN Image Classification')
 st.text('Upload an image for classification')
 
 # ฟังก์ชันสำหรับประมวลผลภาพ
 def process_image(image):
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-    img_array = np.asarray(image)
-    img_array = img_array / 255.0  # ปรับสเกลให้อยู่ในช่วง 0-1
-    img_array = np.expand_dims(img_array, axis=0)
-    return img_array
+    size = 224
+    preprocess = transforms.Compose([
+        transforms.Resize(size),
+        transforms.CenterCrop(size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    img = preprocess(image)
+    img = img.unsqueeze(0)
+    return img
 
 # โค้ดของแอป
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -28,8 +33,10 @@ if uploaded_file is not None:
     # ประมวลผลภาพเมื่อกดปุ่ม
     if st.button('Classify'):
         processed_img = process_image(image)
-        prediction = model.predict(processed_img)
-        classes = ['Alaxan', 'Bactidol', 'Decolgen', 'Fish_Oil', 'Kremil_S']  # แทนที่ด้วยชื่อคลาสของคุณ
-        class_idx = np.argmax(prediction)
-        st.write(f'Prediction: {classes[class_idx]}')
-        st.write(f'Probability: {prediction[0][class_idx]:.2%}')
+        with torch.no_grad():
+            prediction = model(processed_img)
+            probabilities = torch.nn.functional.softmax(prediction[0], dim=0)
+            top_prob, top_class = probabilities.topk(1)
+            classes = ['Alaxan', 'Bactidol', 'Decolgen', 'Fish_Oil', 'Kremil_S']  # แทนที่ด้วยชื่อคลาสของคุณ
+            st.write(f'Prediction: {classes[top_class]}')
+            st.write(f'Probability: {top_prob.item():.2%}')
