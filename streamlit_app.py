df6ea0b814f5771cb4554a41de8db9043cd75202
Suphawit meesak <meepalm32@gmail.com>
2023-12-07 13:52:06 +0700

    new model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-import torch
+import tensorflow as tf
 from PIL import Image
 from prediction import pred_class
 import numpy as np
@@ -7,15 +7,11 @@ import numpy as np
 # Set title 
 st.title('Pharmaceutical Drugs Classification')
 
-#Set Header 
-st.header('Please up load picture')
+# Set Header 
+st.header('Please upload a picture')
 
-
-#Load Model 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = torch.load('modelmobilenetv3.h5', map_location=device)
-
-
+# Load Model 
+model = tf.keras.models.load_model('modelmobilenetv3.h5')
 
 # Display image & Prediction 
 uploaded_image = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
@@ -27,16 +23,20 @@ if uploaded_image is not None:
     class_name = ['Alaxan', 'Bactidol', 'Decolgen', 'Fish_Oil', 'Kremil_S']
 
     if st.button('Prediction'):
-        #Prediction class
-        probli = pred_class(model,image,class_name)
+        # Resize and preprocess the image for prediction
+        img_array = np.array(image.resize((224, 224)))
+        img_array = img_array / 255.0  # Normalize the image
+        
+        # Make prediction
+        predictions = model.predict(np.expand_dims(img_array, axis=0))
+        probli = predictions.tolist()[0]
         
         st.write("## Prediction Result")
-        # Get the index of the maximum value in probli[0]
-        max_index = np.argmax(probli[0])
+        # Get the index of the maximum value in probli
+        max_index = np.argmax(probli)
 
         # Iterate over the class_name and probli lists
         for i in range(len(class_name)):
             # Set the color to blue if it's the maximum value, otherwise use the default color
             color = "blue" if i == max_index else None
-            st.write(f"## <span style='color:{color}'>{class_name[i]} : {probli[0][i]*100:.2f}%</span>", unsafe_allow_html=True)
-
+            st.write(f"## <span style='color:{color}'>{class_name[i]} : {probli[i]*100:.2f}%</span>", unsafe_allow_html=True)
